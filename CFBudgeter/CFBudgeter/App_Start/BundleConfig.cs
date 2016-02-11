725172c71d787e33e7daaed95a2ea0fcb50e3aab
Powers Wartman <powers.wartman@gmail.com>
2016-02-11 12:33:58 -0500

    Lunchtime Commit
    Scaffolded items for model. Added login/out funtionality to layout. Got
    layout functioning

--- CFBudgeter/CFBudgeter/App_Start/BundleConfig.cs
+++ CFBudgeter/CFBudgeter/App_Start/BundleConfig.cs
@@ -8,24 +8,41 @@ namespace CFBudgeter
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js",
+            //          "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
+            bundles.UseCdn = true;
+            var googleFontPath = "http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,800italic,400,600,800";
+
+
+            bundles.Add(new StyleBundle("~/Assets/css", 
+                googleFontPath).Include(
+                       
+                      "~/css/bootstrap.min.css",
+                      "~/js/libs/css/ui-lightness/jquery-ui-1.9.2.custom.css",
+                      "~/css/Apps.css",
+                      "~/css/custom.css"));
+
+            bundles.Add(new ScriptBundle("~/Assets/js").Include(
+                      "~/js/libs/jquery-1.9.1.min.js",
+                      "~/js/libs/jquery-ui-1.9.2.custom.min.js",
+                      "~/js/libs/bootstrap.min.js",
+                      "~/js/App.js"));
         }
     }
 }
