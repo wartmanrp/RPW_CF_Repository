344a2943da9454e4bc74a2067b260f1c0a6680b3
Powers Wartman <powers.wartman@gmail.com>
2016-03-07 13:57:44 -0500

    Mid Day Commit

--- BugSquish/BugSquish/Models/ManageViewModels.cs
+++ BugSquish/BugSquish/Models/ManageViewModels.cs
@@ -90,5 +90,10 @@ namespace BugSquish.Models
         public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public bool HasPassword { get; set; }
+        public IList<UserLoginInfo> Logins { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool TwoFactor { get; set; }
+        public bool BrowserRemembered { get; set; }
     }
 }
