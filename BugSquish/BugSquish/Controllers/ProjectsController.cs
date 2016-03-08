f5fe70d647b7aee81806c7dd3f42208ad27a76a2
Powers Wartman <powers.wartman@gmail.com>
2016-03-07 23:53:12 -0500

    Night commit

--- BugSquish/BugSquish/Controllers/ProjectsController.cs
+++ BugSquish/BugSquish/Controllers/ProjectsController.cs
@@ -15,14 +15,17 @@ namespace BugSquish.Controllers
         private ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: Projects
+        [Authorize(Roles = "Admin")]
         public ActionResult Index()
         {
             var user = db.Users.FirstOrDefault(u => u.UserName == User.Identity.Name);
+
             var projects = db.Projects.Include(p => p.Manager);
             return View(projects.ToList());
         }
 
         // GET: Projects/Details/5
+        [Authorize(Roles = "Admin")]
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -38,6 +41,7 @@ namespace BugSquish.Controllers
         }
 
         // GET: Projects/Create
+        [Authorize(Roles = "Admin")]
         public ActionResult Create()
         {
             ViewBag.ManagerId = new SelectList(db.Users, "Id", "FirstName");
@@ -47,6 +51,7 @@ namespace BugSquish.Controllers
         // POST: Projects/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize(Roles = "Admin")]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,ManagerId,Name,Description")] Project project)
@@ -63,6 +68,7 @@ namespace BugSquish.Controllers
         }
 
         // GET: Projects/Edit/5
+        [Authorize(Roles = "Admin")]
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -81,6 +87,7 @@ namespace BugSquish.Controllers
         // POST: Projects/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize(Roles = "Admin")]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Id,ManagerId,Name,Description")] Project project)
@@ -96,6 +103,7 @@ namespace BugSquish.Controllers
         }
 
         // GET: Projects/Delete/5
+        [Authorize(Roles = "Admin")]
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -111,6 +119,7 @@ namespace BugSquish.Controllers
         }
 
         // POST: Projects/Delete/5
+        [Authorize(Roles = "Admin")]
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
